541fd2f49d874bc731782f553e759847bd447ceb
David Claux <dclaux@microsoft.com>
2015-03-30 11:58:21 -0700

    Use MIT license header

--- Security/SecurityTimestamp.cs
+++ Security/SecurityTimestamp.cs
@@ -1,22 +1,27 @@
-// ---------------------------------------------------------------------------
-// <copyright file="SecurityTimestamp.cs" company="Microsoft">
-//     Copyright (c) Microsoft Corporation.  All rights reserved.
-// </copyright>
-// ---------------------------------------------------------------------------
-
-//-----------------------------------------------------------------------
-// <summary>
-// Defines the SecurityTimestamp class.  Note that this file is
-// excerpted from WCF sources (ndp\indigo\src\ServiceModel\
-// System\ServiceModel\Security\SecurityTimestamp.cs).
-// </summary>
-//-----------------------------------------------------------------------
-
-// ********************************************************************************
-// * NOTE: As noted above, this file is excerpted directly from the WCF source    *
-// * code base.  Please do NOT modify this file so as to make keeping it in sync  *
-// * with the WCF sources easier.                                                 *
-// ********************************************************************************
+/*
+ * Exchange Web Services Managed API
+ *
+ * Copyright (c) Microsoft Corporation
+ * All rights reserved.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+ * to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
 
 namespace Microsoft.Exchange.WebServices.Data
 {
